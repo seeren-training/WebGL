f427e8bef8a5bce3dcf009283acb6c1de09f6d5b
seeren <consultant@seeren.fr>
2021-11-24 11:10:50 +0100

    Add gui controls

--- src/components/scenes/getting-started.scene.js
+++ src/components/scenes/getting-started.scene.js
@@ -15,12 +15,25 @@ export class GettingStartedScene extends Scene {
             new BoxGeometry(2, 2, 2),
             new MeshBasicMaterial()
         ));
-        camera.position.set(0, 2, 5);
-        camera.lookAt(cube.position);
         this.animate.attach(() => {
             cube.rotation.x += .01;
             cube.rotation.y += .01;
         })
+        this.gui && this.#gui(animate);
+        camera.position.set(0, 2, 5);
+        camera.lookAt(cube.position);
+    }
+
+    /**
+     * @param {AnimateEvent} animate 
+     */
+    #gui(animate) {
+        this.gui
+            .add({ animate: true }, 'animate')
+            .onChange((value) => !value ? animate.disable() : animate.enable());
+        this.gui
+            .add({ fps: 30 }, 'fps', 0, 60)
+            .onChange((value) => animate.fps = value)
     }
 
 }
